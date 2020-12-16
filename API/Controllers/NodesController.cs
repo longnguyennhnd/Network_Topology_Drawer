1bdc8c2f45de2465ac6684af0f7f89b9d500676f
Long Nguyen <longnguyennhnd@gmail.com>
2020-12-16 08:38:50 +0700

    commited

---
+++ API/Controllers/NodesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    [EnableCors(origins: "http://localhost:52211", headers: "*", methods: "*")]
+    public class NodesController : ApiController
+    {
+        private topologyEntities2 db = new topologyEntities2();
+
+        // GET: api/Nodes
+        [HttpGet]
+        public List<Node> GetNode()
+        {
+            return db.Nodes.ToList();
+        }
+
+        // GET: api/Nodes/5
+        [ResponseType(typeof(Node))]
+        public IHttpActionResult GetNode(string id)
+        {
+            Node node = db.Nodes.Find(id);
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(node);
+        }
+
+        // PUT: api/Nodes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutNode(string id, Node node)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != node.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(node).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NodeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Nodes
+        [ResponseType(typeof(Node))]
+        public IHttpActionResult PostNode(Node node)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Nodes.Add(node);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (NodeExists(node.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = node.ID }, node);
+        }
+
+        // DELETE: api/Nodes/5
+        [ResponseType(typeof(Node))]
+        public IHttpActionResult DeleteNode(string id)
+        {
+            Node node = db.Nodes.Find(id);
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            db.Nodes.Remove(node);
+            db.SaveChanges();
+
+            return Ok(node);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool NodeExists(string id)
+        {
+            return db.Nodes.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ API/Scripts/network_topology.js
@@ -0,0 +1,160 @@
+﻿
+    var edges = new vis.DataSet([]);
+    var nodes = new vis.DataSet([]);
+
+        $(document).ready(function () {
+            var listNode = [];
+            var listEdge = [];
+
+            $.ajax({
+        url: "https://localhost:44344/api/Nodes",
+                type: 'GET',
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+        console.log(data);
+                    data.forEach(val => {
+                        var node = {
+        id: val.ID,
+                            shape: "image",
+                            size: 20,
+                            label: val.nameNode,
+                            image: "../img/" + val.image,
+                            parent: val.parent
+                        };
+                        var edge = {
+        id: val.ID,
+                            from: val.ID,
+                            to: val.parent
+                        };
+                        edges.add(edge);
+                        nodes.add(node);
+
+                    });
+
+                },
+            })
+            var container = document.getElementById("mynetwork");
+
+            // provide the data in the vis format
+            var data = {
+        nodes: nodes,
+                edges: edges
+            };
+
+            var options = {
+        layout: {
+        randomSeed: 5,
+                },
+                nodes: {
+        brokenImage: "./broken-image.png",
+                },
+            };
+            // initialize!
+            var network = new vis.Network(container, data, options);
+            network.on('click', function (properties) {
+                var ids = properties.nodes;
+                var clickedNodes = nodes.get(ids);
+                console.log(clickedNodes);
+                document.getElementById("node-id").setAttribute('value', clickedNodes[0].id);
+                document.getElementById("node-label").setAttribute('value', clickedNodes[0].label);
+                document.getElementById("node-image").setAttribute('value', clickedNodes[0].image);
+                document.getElementById("node-parent").setAttribute('value', clickedNodes[0].parent);
+            });
+
+        });
+        function addNode() {
+            try {
+        let tmp = {
+        ID: document.getElementById("node-id").value,
+                    nameNode: document.getElementById("node-label").value,
+                    image: document.getElementById("node-image").value,
+                    parent: document.getElementById("node-parent").value
+                };
+
+                $.ajax({
+        type: 'POST',
+                    url: 'https://localhost:44344/api/Nodes',
+                    data: tmp,
+                    dataType: 'json',
+                    success: function (data) {
+        console.log(data);
+                        nodes.add({
+        id: document.getElementById("node-id").value,
+                            shape: "image",
+                            size: 20,
+                            label: document.getElementById("node-label").value,
+                            image: "../img/" + document.getElementById("node-image").value,
+                            parent: document.getElementById("node-parent").value,
+                        });
+                        edges.add({
+        id: document.getElementById("node-id").value,
+                            from: document.getElementById("node-id").value,
+                            to: document.getElementById("node-parent").value
+                        });
+                    },
+                    error: function (data) {
+        console.log('Error', data);
+                    }
+                });
+            } catch (err) {
+        console.log(err);
+            }
+        }
+        function updateNode() {
+            try {
+        let tmp = {
+        ID: document.getElementById("node-id").value,
+                    nameNode: document.getElementById("node-label").value,
+                    image: document.getElementById("node-image").value,
+                    parent: document.getElementById("node-parent").value
+                };
+
+                $.ajax({
+        type: 'PUT',
+                    url: 'https://localhost:44344/api/Nodes/' + document.getElementById("node-id").value,
+                    data: tmp,
+                    dataType: 'json',
+                    success: function (data) {
+        console.log(data);
+                        nodes.update({
+        id: document.getElementById("node-id").value,
+                            shape: "image",
+                            size: 20,
+                            label: document.getElementById("node-label").value,
+                            image: "../img/" + document.getElementById("node-image").value,
+                            parent: document.getElementById("node-parent").value,
+                        });
+                        edges.update({
+        id: document.getElementById("node-id").value,
+                            from: document.getElementById("node-id").value,
+                            to: document.getElementById("node-parent").value
+                        });
+                    },
+                    error: function (data) {
+        console.log('Error', data);
+                    }
+                });
+            } catch (err) {
+        console.log(err);
+            }
+        }
+        function removeNode() {
+            try {
+
+        $.ajax({
+            type: 'DELETE',
+            url: 'https://localhost:44344/api/Nodes/' + document.getElementById("node-id").value,
+            dataType: 'json',
+            success: function (data) {
+                nodes.remove({ id: document.getElementById("node-id").value });
+                edge.remove({ id: document.getElementById("node-id").value });
+            },
+            error: function (data) {
+                console.log('Error', data);
+            }
+        });
+            } catch (err) {
+        alert(err);
+            }
+        }
